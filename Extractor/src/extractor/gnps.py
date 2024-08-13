af5703fa1ab090f56c11375060b8b8ba9233e6f3
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-13 16:07:13 +0200

    Extracting job

--- Extractor/src/extractor/gnps.py
+++ Extractor/src/extractor/gnps.py
@@ -3,8 +3,10 @@ import os
 import requests
 import json
 from pathlib import Path
+import xml.etree.ElementTree as ET
+from collections import Counter
 
-class GnpsFile:
+class GnpsAnnotationsFile:
     def __init__(self, json_file: str | os.PathLike):
         self.json_file = json_file
 
@@ -17,8 +19,28 @@ class GnpsFile:
             return df
     
     def summary(self):
-        return self.df().loc[:, ["#Scan#", "Adduct", "ExactMass", "Precursor_MZ", "SpecMZ", "Charge", "SpecCharge", "INCHI", "INCHI_AUX", "InChIKey"]]
-    
+        summary = self.df().loc[:, ["#Scan#", "Adduct", "ExactMass", "Precursor_MZ", "SpecMZ", "Charge", "SpecCharge", "MQScore", "INCHI", "INCHI_AUX", "InChIKey"]].rename(columns = {"#Scan#": "Id"})
+        summary["Id"] = summary["Id"].astype(int)
+        return summary.set_index("Id").sort_index()
+
+class GnpsParametersFile:
+    def __init__(self, xml_file: str | os.PathLike):
+        self.xml_file = xml_file
+
+    def params(self):
+        with open(self.xml_file) as f:
+            xml = f.read()
+            tree = ET.parse(self.xml_file)
+            root = tree.getroot()
+            subtags = set([p.tag for p in root])
+            assert subtags == {"parameter"}, subtags
+            names_mult = Counter([p.attrib["name"] for p in root])
+            dupls = set([names for names, count in names_mult.items() if count > 1])
+            assert dupls == {} or dupls == {"upload_file_mapping"}, dupls
+
+            d = {p.attrib["name"]: p.text for p in root if p.attrib["name"] not in dupls}
+            return d
+        
 class GnpsFetcher:
     def fetch(task_id: str):
         url = f"https://gnps.ucsd.edu/ProteoSAFe/result_json.jsp?task={task_id}&view=view_all_annotations_DB"
@@ -30,6 +52,16 @@ class GnpsFetcher:
         with open(path, "wb") as f:
             f.write(GnpsFetcher.fetch(task_id))
 
+    def fetch_parameters(task_id: str):
+        url = f"https://gnps.ucsd.edu/ProteoSAFe/ManageParameters?task={task_id}"
+        with requests.get(url) as r:
+            r.raise_for_status()
+            return r.content
+
+    def fetch_parameters_and_save(task_id: str, path: str | os.PathLike):
+        with open(path, "wb") as f:
+            f.write(GnpsFetcher.fetch_parameters(task_id))
+    
 class GnpsCacher:
     CACHE_DIR = Path("fetched")
 
@@ -39,3 +71,25 @@ class GnpsCacher:
         if not path.exists():
             GnpsFetcher.fetch_and_save(task_id, path)
         return path
+    
+    def cache_retrieve_parameters(task_id: str):
+        os.makedirs(GnpsCacher.CACHE_DIR, exist_ok=True)
+        path = GnpsCacher.CACHE_DIR / f"{task_id}.xml"
+        if not path.exists():
+            GnpsFetcher.fetch_parameters_and_save(task_id, path)
+        return path
+    
+class GnpsInchiScore:
+    def __init__(self, all_annotations: Path, parameters: Path):
+        self.summary = GnpsAnnotationsFile(all_annotations).summary()
+        ps = GnpsParametersFile(parameters).params()
+        self.min_peaks = int(ps["MIN_MATCHED_PEAKS"])
+        self.max_delta_mass = float(ps["tolerance.PM_tolerance"])
+    
+    @property
+    def inchis(self):
+        return self.summary.loc[:, "INCHI"]
+    
+    @property
+    def scores(self):
+        return self.summary.loc[:, "MQScore"]
--- Extractor/tests/test_various.py
+++ Extractor/tests/test_various.py
@@ -1,9 +1,11 @@
 import pandas as pd
-from extractor.gnps import GnpsFile
+from extractor.gnps import GnpsAnnotationsFile
+from extractor.gnps import GnpsParametersFile
 import json
+import xml.etree.ElementTree as ET
 
 def test_read_df():
-    df = GnpsFile("tests/resources/26a5cbca3e844cc0b126f992c69df832.json").df()
+    df = GnpsAnnotationsFile("tests/resources/26a5cbca3e844cc0b126f992c69df832.json").df()
     assert df.shape == (71, 48)
     
 def test_read_df_manual():
@@ -14,3 +16,19 @@ def test_read_df_manual():
         (k, v), = js.items()
         df = pd.DataFrame(v)
     assert df.shape == (71, 48)
+
+def test_parse_parameters():
+    xml_file = "tests/resources/26a5cbca3e844cc0b126f992c69df832.xml"
+    ps = GnpsParametersFile(xml_file).params()
+    assert len(ps) == 38
+
+def test_parse_parameters_manual():
+    xml_file = "tests/resources/26a5cbca3e844cc0b126f992c69df832.xml"
+    with open(xml_file) as f:
+        xml = f.read()
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        p0 = root[0]
+        assert p0.tag == "parameter"
+        assert p0.attrib["name"] == "ANALOG_SEARCH"
+
