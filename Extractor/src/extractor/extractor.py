039b324b9e55934f15819931118a0f0656f7c476
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-13 11:13:57 +0200

    Parse json

--- Extractor/src/extractor/extractor.py
+++ Extractor/src/extractor/extractor.py
@@ -1,12 +1,9 @@
 import os
 import pandas as pd
 from extractor.gnps import GnpsFile
-from extractor.gnps import GnpsFetcher
+from extractor.gnps import GnpsCacher
 
 def main():
-    # Create dir if does not exist
-    os.makedirs("fetched", exist_ok=True)
-    print("Downloading.")
-    GnpsFetcher.fetch_and_save("26a5cbca3e844cc0b126f992c69df832", "fetched/26a5cbca3e844cc0b126f992c69df832.json")
-    print("Downloaded.")
-
+    p = GnpsCacher.cache_retrieve("26a5cbca3e844cc0b126f992c69df832")
+    df = GnpsFile(p).df()
+    print(df)
--- Extractor/src/extractor/gnps.py
+++ Extractor/src/extractor/gnps.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import requests
 import json
+from pathlib import Path
 
 class GnpsFile:
     def __init__(self, json_file: str | os.PathLike):
@@ -15,6 +16,9 @@ class GnpsFile:
             df = pd.DataFrame(v)
             return df
     
+    def summary(self):
+        return self.df().loc[:, ["#Scan#", "Adduct", "ExactMass", "Precursor_MZ", "SpecMZ", "Charge", "SpecCharge", "INCHI", "INCHI_AUX", "InChIKey"]]
+    
 class GnpsFetcher:
     def fetch(task_id: str):
         url = f"https://gnps.ucsd.edu/ProteoSAFe/result_json.jsp?task={task_id}&view=view_all_annotations_DB"
@@ -25,3 +29,13 @@ class GnpsFetcher:
     def fetch_and_save(task_id: str, path: str | os.PathLike):
         with open(path, "wb") as f:
             f.write(GnpsFetcher.fetch(task_id))
+
+class GnpsCacher:
+    CACHE_DIR = Path("fetched")
+
+    def cache_retrieve(task_id: str):
+        os.makedirs(GnpsCacher.CACHE_DIR, exist_ok=True)
+        path = GnpsCacher.CACHE_DIR / f"{task_id}.json"
+        if not path.exists():
+            GnpsFetcher.fetch_and_save(task_id, path)
+        return path
