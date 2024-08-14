42fc56f6f4a985dc60dd9a30c303eb04e50fd503
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-14 18:11:05 +0200

    Cleaned up export

--- Extractor/src/extractor/extractor.py
+++ Extractor/src/extractor/extractor.py
@@ -49,45 +49,9 @@ for id in compounds["Id"]:
     spectrum = d[name]
     all_spectra.append(spectrum)
 
-matchms.exporting.save_as_mgf(all_spectra, str(output_dir / "All Matchms.mgf"))
-matchms.exporting.save_as_mgf(all_spectra, str(output_dir / "All Gnps.mgf"), export_style="gnps")
-
-s = next(iter(d.values()))
-s.get("precursor_mz")
-
-# kf_from_inchi = Chem.inchi.MolFromInchi(
-#     "InChI=1S/C15H10O6/c16-8-3-1-7(2-4-8)15-14(20)13(19)12-10(18)5-9(17)6-11(12)21-15/h1-6,16-18,20"
-# )
-hl_from_inchi = Chem.inchi.MolFromInchi(
-    "InChI=1S/C15H24N2O2/c18-12-4-5-16-8-10-6-11(14(16)7-12)9-17-13(10)2-1-3-15(17)19/h10-14,18H,1-9H2/t10-,11-,12-,13+,14-/m0/s1"
-)
-Descriptors.MolWt(hl_from_inchi)
-
-# Draw.MolToImage(hl_from_inchi).save("hl_from_inchi.png")
-canon_smiles = pyAvalonTools.GetCanonSmiles(hl_from_inchi)
-hl_from_canon_smiles = Chem.MolFromSmiles(canon_smiles)
-# Draw.MolToImage(hl_from_canon_smiles).save("hl_from_canon_smiles.png")
-hl_from_smiles = Chem.MolFromSmiles("[C@@H]12CN3CC[C@H](O)C[C@H]3[C@H](CN3C(=O)CCC[C@H]23)C1")
-# Draw.MolToImage(hl_from_smiles).save("hl_from_smiles.png")
-
-d = rdMolDraw2D.MolDraw2DSVG(500, 500)
-s = rdMolDraw2D.PrepareAndDrawMolecule(d, hl_from_inchi)
-d.FinishDrawing()
-t = d.GetDrawingText()
-# with open("hl_from_inchi.svg", "w") as f:
-# f.write(t)
-
-t = Draw.MolToACS1996SVG(hl_from_inchi)
-# with open("hl_from_inchi_acs1996.svg", "w") as f:
-# f.write(t)
-
-# Draw.MolToSVG(hl_from_inchi)
-# ValueError: Bad Conformer Id
-# Attempt as per https://github.com/rdkit/rdkit/issues/4991
-# rdDepictor.Compute2DCoords(hl_from_inchi)
-# Draw.MolToSVG(hl_from_inchi)
-# ValueError: Bad Conformer Id
-
+matchms.exporting.save_as_mgf(all_spectra, str(output_dir / "All.mgf"))
+# export_style="gnps" does not export the feature_ids. It does export PEPMASS, but apparently having the charge and the precursor_mz is ok to GNPS as well.
+# matchms.exporting.save_as_mgf(all_spectra, str(output_dir / "All Gnps.mgf"), export_style="gnps")
 
 task_ids_file = "../Manufactured case/Gnps task ids.json"
 with open(task_ids_file) as task_ids_data:
@@ -105,4 +69,33 @@ for task_id in task_ids:
     assert isc.scores.index == compounds.index
     compounds.assign(**new_cols)
 
-# def main():
+
+def to_image():
+    hl_from_inchi = Chem.inchi.MolFromInchi(
+        "InChI=1S/C15H24N2O2/c18-12-4-5-16-8-10-6-11(14(16)7-12)9-17-13(10)2-1-3-15(17)19/h10-14,18H,1-9H2/t10-,11-,12-,13+,14-/m0/s1"
+    )
+
+    # Draw.MolToImage(hl_from_inchi).save("hl_from_inchi.png")
+    canon_smiles = pyAvalonTools.GetCanonSmiles(hl_from_inchi)
+    hl_from_canon_smiles = Chem.MolFromSmiles(canon_smiles)
+    # Draw.MolToImage(hl_from_canon_smiles).save("hl_from_canon_smiles.png")
+    hl_from_smiles = Chem.MolFromSmiles("[C@@H]12CN3CC[C@H](O)C[C@H]3[C@H](CN3C(=O)CCC[C@H]23)C1")
+    # Draw.MolToImage(hl_from_smiles).save("hl_from_smiles.png")
+
+    d = rdMolDraw2D.MolDraw2DSVG(500, 500)
+    s = rdMolDraw2D.PrepareAndDrawMolecule(d, hl_from_inchi)
+    d.FinishDrawing()
+    t = d.GetDrawingText()
+    # with open("hl_from_inchi.svg", "w") as f:
+    # f.write(t)
+
+    t = Draw.MolToACS1996SVG(hl_from_inchi)
+    # with open("hl_from_inchi_acs1996.svg", "w") as f:
+    # f.write(t)
+
+    # Draw.MolToSVG(hl_from_inchi)
+    # ValueError: Bad Conformer Id
+    # Attempt as per https://github.com/rdkit/rdkit/issues/4991
+    # rdDepictor.Compute2DCoords(hl_from_inchi)
+    # Draw.MolToSVG(hl_from_inchi)
+    # ValueError: Bad Conformer Id
