2a68d5b3b16150a791259cc4b79cae53e9a92607
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-14 16:00:51 +0200

    Rm titles code

--- Extractor/src/extractor/extractor.py
+++ Extractor/src/extractor/extractor.py
@@ -5,18 +5,81 @@ from extractor.gnps import GnpsAnnotationsFile
 from extractor.gnps import GnpsCacher
 from extractor.gnps import GnpsParametersFile
 from extractor.gnps import GnpsInchiScore
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import Descriptors
+from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit.Avalon import pyAvalonTools
+from rdkit.Chem.Draw import rdDepictor
+import matchms
+from pathlib import Path
+
+p = Path("../Manufactured case/Mgf files/")
+
+b = list(matchms.importing.load_from_mgf("../Manufactured case/Mgf files/3-beta-amino-pregnane.mgf"))
+# list files in the directory
+l = os.listdir(p)
+for file_name in l:
+    path = p / file_name
+    with open(path) as f:
+        lines = f.readlines()
+        lines = [l for l in lines if not l.startswith("TITLE")]
+    with open(path, "w") as f:
+        f.writelines(lines)
+
+# kf_from_inchi = Chem.inchi.MolFromInchi(
+#     "InChI=1S/C15H10O6/c16-8-3-1-7(2-4-8)15-14(20)13(19)12-10(18)5-9(17)6-11(12)21-15/h1-6,16-18,20"
+# )
+hl_from_inchi = Chem.inchi.MolFromInchi(
+    "InChI=1S/C15H24N2O2/c18-12-4-5-16-8-10-6-11(14(16)7-12)9-17-13(10)2-1-3-15(17)19/h10-14,18H,1-9H2/t10-,11-,12-,13+,14-/m0/s1"
+)
+Descriptors.MolWt(hl_from_inchi)
+
+# Draw.MolToImage(hl_from_inchi).save("hl_from_inchi.png")
+canon_smiles = pyAvalonTools.GetCanonSmiles(hl_from_inchi)
+hl_from_canon_smiles = Chem.MolFromSmiles(canon_smiles)
+# Draw.MolToImage(hl_from_canon_smiles).save("hl_from_canon_smiles.png")
+hl_from_smiles = Chem.MolFromSmiles("[C@@H]12CN3CC[C@H](O)C[C@H]3[C@H](CN3C(=O)CCC[C@H]23)C1")
+# Draw.MolToImage(hl_from_smiles).save("hl_from_smiles.png")
+
+d = rdMolDraw2D.MolDraw2DSVG(500, 500)
+s = rdMolDraw2D.PrepareAndDrawMolecule(d, hl_from_inchi)
+d.FinishDrawing()
+t = d.GetDrawingText()
+# with open("hl_from_inchi.svg", "w") as f:
+# f.write(t)
+
+t = Draw.MolToACS1996SVG(hl_from_inchi)
+# with open("hl_from_inchi_acs1996.svg", "w") as f:
+# f.write(t)
+
+# Draw.MolToSVG(hl_from_inchi)
+# ValueError: Bad Conformer Id
+# Attempt as per https://github.com/rdkit/rdkit/issues/4991
+# rdDepictor.Compute2DCoords(hl_from_inchi)
+# Draw.MolToSVG(hl_from_inchi)
+# ValueError: Bad Conformer Id
 
 compounds_file = "../Manufactured case/Compounds.tsv"
-compounds = pd.read_csv(compounds_file, sep="\t").set_index("New id")
+compounds = pd.read_csv(compounds_file, sep="\t").set_index("Id")
+# compounds["Rdkit_molecular_weight"] = compounds.loc[compounds.index <= 1, ["InChI"]].apply(
+#     lambda i: Descriptors.HeavyAtomMolWt(Chem.inchi.MolFromInchi(i)), axis=1
+# )
+inchs = compounds.loc[compounds["Exact mass"].notna(), "InChI"]
+compounds["Rdkit molecular weight"] = inchs.apply(lambda i: Descriptors.HeavyAtomMolWt(Chem.inchi.MolFromInchi(i)))
+
 task_ids_file = "../Manufactured case/Gnps task ids.json"
 with open(task_ids_file) as task_ids_data:
     task_ids = json.load(task_ids_data)
 
-for task_id in task_ids: 
+for task_id in task_ids:
     p = GnpsCacher.cache_retrieve(task_id)
     x = GnpsCacher.cache_retrieve_parameters(task_id)
     isc = GnpsInchiScore(p, x)
-    new_cols = {f"inchi_gnps_{isc.min_peaks}_{isc.max_delta_mass}": isc.inchis, f"score_gnps_{isc.min_peaks}_{isc.max_delta_mass}": isc.scores}
+    new_cols = {
+        f"inchi_gnps_{isc.min_peaks}_{isc.max_delta_mass}": isc.inchis,
+        f"score_gnps_{isc.min_peaks}_{isc.max_delta_mass}": isc.scores,
+    }
     assert isc.inchis.index == compounds.index
     assert isc.scores.index == compounds.index
     compounds.assign(**new_cols)
