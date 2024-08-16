37ffee7f0b700c4961fab0c99dfe939389131d0d
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-16 14:14:37 +0200

    Send & sent tasks

--- Extractor/src/extractor/sender.py
+++ Extractor/src/extractor/sender.py
@@ -1,11 +1,22 @@
 from ms2decide.ClosestGNPS import _launch_GNPS_workflow_iterative
+from ms2decide.ClosestGNPS import _upload_to_gnps
 from ms2decide.AuthMail import AuthMail
 from datetime import datetime
 from pathlib import Path
+import json
 
 input_dir = Path("../Generated/") / "Manufactured case/"
 input_mgf = str((input_dir / "All GNPS.mgf").resolve())
 input_quant = str((input_dir / "Quantification table.csv").resolve())
-title = f"Manufactured case {datetime.now().isoformat()}"
 auth = AuthMail.from_txt("../../../Auth GNPS.txt")
-d = _launch_GNPS_workflow_iterative(auth, input_mgf, input_quant, title)
+title = f"Manufactured case {datetime.now().isoformat()}"
+(gnps_input_mgf, gnps_input_csv) = _upload_to_gnps(auth, input_mgf, input_quant, title)
+d = _launch_GNPS_workflow_iterative(auth, gnps_input_mgf, gnps_input_csv, title)
+
+# ids_by_first = {k:[v.split('=')[1] for kk,v in d.items()] for k,d in d_jobs.items()}
+ids_by_first = {k:[v for kk,v in e.items()] for k,e in d.items()}
+all_ids = [v for d in ids_by_first.values() for v in d]
+# write to json file
+with open(input_dir/"Gnps task ids.json", 'w') as f:
+    f.write(json.dumps(ids_by_first))
+print(json.dumps(all_ids))
