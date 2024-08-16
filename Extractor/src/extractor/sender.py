52b61935c59a8ad04aeca73f6934ea903fc12602
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-16 12:04:31 +0200

    spc

--- Extractor/src/extractor/sender.py
+++ Extractor/src/extractor/sender.py
@@ -2,6 +2,7 @@ from ms2decide.ClosestGNPS import _launch_GNPS_workflow_iterative
 from ms2decide.AuthMail import AuthMail
 from datetime import datetime
 from pathlib import Path
+
 input_dir = Path("../Generated/") / "Manufactured case/"
 input_mgf = str((input_dir / "All GNPS.mgf").resolve())
 input_quant = str((input_dir / "Quantification table.csv").resolve())
