d1e31f6b806a582eb193799d3261a620fec730c1
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-17 18:44:44 +0200

    Ren folder code

--- Extractor/src/extractor/pleiocarpa/datadirs.py
+++ Extractor/src/extractor/pleiocarpa/datadirs.py
@@ -9,5 +9,5 @@ GENERATED_DIR_SUMMARY = GENERATED_DIR / "7 - Summary/"
 GENERATED_DIR_ARTICLE = REPO_DIR / "../" / "Article/" / "Generated/"
 
 INPUT_DIR = REPO_DIR / "Pleiocarpa case/"
-INPUT_DIR_GNPS_TASKS = INPUT_DIR / "4- GNPS iterative/"
+INPUT_DIR_GNPS_TASKS = INPUT_DIR / "4 - GNPS iterative/"
 INPUT_DIR_SIRIUS = INPUT_DIR / "5 - Sirius/"
