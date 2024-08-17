d521afb1372ecc4eab87803e3a9ad2550850bbe9
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-08-17 18:46:29 +0200

    Gen more Pleio

--- Extractor/src/extractor/pleiocarpa/extractor.py
+++ Extractor/src/extractor/pleiocarpa/extractor.py
@@ -8,6 +8,7 @@ from extractor.gnps import GnpsParametersFile
 from extractor.gnps import GnpsInchiScore
 from extractor.gnps import GnpsTasks
 from extractor.mgfs import MgfFiles
+from extractor.support import add_ranks_columns
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 import matchms
@@ -56,8 +57,85 @@ def generate_summary():
         .rename(columns={"InChI": "InChI ISDB-LOTUS", "Score": "Score ISDB-LOTUS"})
     )
     compounds_joined = compounds_joined.join(isdb_df)
+    
+    ids = compounds_joined.index
 
-    compounds_joined.to_csv(GENERATED_DIR_SUMMARY / "All matches.tsv", sep="\t")
+    compounds_joined["cg"] = compounds_joined["Score GNPS iterated discounted"].fillna(0)
+    compounds_joined["cs"] = compounds_joined["Score Sirius"].fillna(0.5)
+    compounds_joined["ci"] = compounds_joined["Score ISDB-LOTUS"].fillna(0)
+
+    tanimotos_by_id = {}
+    tanimoto_values_by_id = {}
+    for id in ids:
+        inchi_g = compounds_joined.loc[id, "InChI GNPS iterated"]
+        inchi_s = compounds_joined.loc[id, "InChI Sirius"]
+        inchi_i = compounds_joined.loc[id, "InChI ISDB-LOTUS"]
+
+        by_tool = {
+            Tool.GNPS.name: inchi_g if not pd.isna(inchi_g) else "*",
+            Tool.SIRIUS.name: inchi_s if not pd.isna(inchi_s) else "*",
+            Tool.ISDB.name: inchi_i if not pd.isna(inchi_i) else "*",
+        }
+        tanimotos = Tanimotos(by_tool)
+        (tgs, tgi, tsi) = tanimotos.compute_tanimoto()
+
+        if pd.isna(inchi_g):
+            assert tgs == 0
+            assert tgi == 0
+        if pd.isna(inchi_s):
+            assert tgs == 0
+            assert tsi == 0
+            tgs = 0.25
+            tsi = 0.25
+        assert not pd.isna(inchi_i)
+        tanimotos_by_id[id] = tanimotos
+        tanimoto_values_by_id[id] = {"tgs": tgs, "tgi": tgi, "tsi": tsi}
+
+    tanimotos_df = pd.DataFrame.from_dict(tanimoto_values_by_id, orient="index")
+    compounds_joined = compounds_joined.join(tanimotos_df)
+
+    k_by_id = {}
+    for id in ids:
+        similarities = {
+            Tool.GNPS.name: compounds_joined.loc[id, "cg"],
+            Tool.SIRIUS.name: compounds_joined.loc[id, "cs"],
+            Tool.ISDB.name: compounds_joined.loc[id, "ci"],
+        }
+        tanimotos = tanimotos_by_id[id]
+        k = K(similarities, tanimotos).k()
+        k_by_id[id] = k
+    k_df = pd.Series(k_by_id, name="K")
+    compounds_joined = compounds_joined.join(k_df)
+
+    add_ranks_columns(
+        compounds_joined,
+        "Rank min GNPS original",
+        "Rank max GNPS original",
+        "Ranks GNPS original",
+        "Score GNPS; peaks ≥ 6; Δ mass ≤ 0.02",
+    )
+    add_ranks_columns(
+        compounds_joined,
+        "Rank min GNPS iterated",
+        "Rank max GNPS iterated",
+        "Ranks GNPS iterated",
+        "Score GNPS iterated discounted",
+    )
+    add_ranks_columns(compounds_joined, "Rank min Sirius", "Rank max Sirius", "Ranks Sirius", "Score Sirius")
+    add_ranks_columns(
+        compounds_joined, "Rank min ISDB-LOTUS", "Rank max ISDB-LOTUS", "Ranks ISDB-LOTUS", "Score ISDB-LOTUS"
+    )
+    add_ranks_columns(compounds_joined, "Rank min K", "Rank max K", "Ranks K", "K")
+
+    compounds_joined.to_csv(GENERATED_DIR_SUMMARY / "Compounds joined.tsv", sep="\t")
+
+    counts = compounds_joined.filter(like="Standard InChI GNPS; ").agg("count")
+    counts.index.name = "Column"
+    counts.name = "Count"
+    counts.to_csv(GENERATED_DIR_SUMMARY / "Counts.tsv", sep="\t")
+
+    by_k = compounds_joined.loc[:, ["cg", "cs", "ci", "tgs", "tgi", "tsi", "K", "Ranks K"]].sort_values("Ranks K")
+    by_k.to_csv(GENERATED_DIR_SUMMARY / "Compounds by K.tsv", sep="\t")
 
 # compute_isdb()
 # transform_isdb()
